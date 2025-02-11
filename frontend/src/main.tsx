a30a7a385afe7dd0467d63fce4e005a42d22d900
yisus82 <djalma8@gmail.com>
2025-02-11 08:58:15 +0100

    Add color mode support from chakra ui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,16 @@
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { Provider } from './components/ui/provider';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <ChakraProvider value={defaultSystem}>
+      <Provider>
         <App />
-      </ChakraProvider>
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
