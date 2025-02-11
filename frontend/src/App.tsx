f62237ef13f2f259a3b5e98ead33d52dfbb6be0d
yisus82 <djalma8@gmail.com>
2025-02-11 12:59:13 +0100

    Navbar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 const App = () => (
-  <Box minH={'100vh'} bg={useColorModeValue('gray.100', 'gray.900')}>
+  <Box minH='100vh' bg={useColorModeValue('gray.100', 'gray.900')} px={4} py={4}>
     <Navbar />
     <Routes>
       <Route path='/' element={<HomePage />} />
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,15 +1,38 @@
+import { Container, Flex, HStack, IconButton, Text } from '@chakra-ui/react';
+import { LuPlus } from 'react-icons/lu';
 import { Link } from 'react-router-dom';
+import { ColorModeButton } from '../ui/color-mode';
 import './Navbar.css';
 
 const Navbar = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link to='/'>Home</Link>
-        <Link to='/create'>Create</Link>
-      </li>
-    </ul>
-  </nav>
+  <Container maxW='1140px' px={4} py={4}>
+    <Flex h={16} alignItems='center' justifyContent='space-between' flexDir='row'>
+      <Text
+        fontSize={{ base: '6xl', smDown: 'xl' }}
+        fontWeight='bold'
+        textTransform='uppercase'
+        textAlign='center'
+        bgGradient='to-r'
+        gradientFrom='cyan.400'
+        gradientTo='blue.500'
+        bgClip='text'
+      >
+        <Link to='/'>MERN Market 🛒</Link>
+      </Text>
+      <HStack spaceX={2} alignItems='center'>
+        <Link to='/create'>
+          <IconButton
+            variant='subtle'
+            aria-label='Create product'
+            size={{ base: '2xl', smDown: 'sm' }}
+          >
+            <LuPlus />
+          </IconButton>
+        </Link>
+        <ColorModeButton variant='subtle' size={{ base: '2xl', smDown: 'sm' }} />
+      </HStack>
+    </Flex>
+  </Container>
 );
 
 export default Navbar;
